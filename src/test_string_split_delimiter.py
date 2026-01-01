e09450cbd298935a6076a9aad73c0d7dad87da8a
its-PKN-2k4 <phuckhanhnguyen2004@gmail.com>
2026-01-01 18:29:40 -0500

    Add testing functions for splitting and creating new nodes for image and link markdowns

--- src/test_string_split_delimiter.py
+++ src/test_string_split_delimiter.py
@@ -1,5 +1,5 @@
 import unittest
-from string_split_delimiter import split_nodes_at_delimiter
+from string_split_delimiter import *
 
 from textnode import TextNode, TextType
 
@@ -82,4 +82,35 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" word", TextType.TEXT),
             ],
             new_nodes,
+        )
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "I buy motherboards from [MSI](https://ca.msi.com/) and [Gigabyte](https://www.gigabyte.com/)", TextType.TEXT
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("I buy motherboards from ", TextType.TEXT),
+                TextNode("MSI", TextType.LINK, "https://ca.msi.com/"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("Gigabyte", TextType.LINK, "https://www.gigabyte.com/")
+            ],
+            new_nodes
         )
