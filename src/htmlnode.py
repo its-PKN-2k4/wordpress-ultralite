d249e6a23370a7fb822d4174a8488b93cf236712
its-PKN-2k4 <phuckhanhnguyen2004@gmail.com>
2026-01-03 02:18:11 -0500

    Move leafHTMLnode and parentHTMLnode classes from separate files to htmlnode.py and delete old files

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,3 +21,30 @@ class HTMLNode:
 
     def __repr__(self):
         return f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})'
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return f'{self.value}'
+        return f'<{self.tag} {super().props_to_html()}>{self.value}</{self.tag}>' if self.props != None else f'<{self.tag}>{self.value}</{self.tag}>'
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError
+        if self.children == None:
+            raise ValueError("No children node founded")
+        children_html = []
+        for child in self.children:
+            children_html.append(child.to_html())
+        children_html = "".join(children_html)
+        final = f'<{self.tag} {super().props_to_html()}>{children_html}</{self.tag}>' if self.props != None else f'<{self.tag}>{children_html}</{self.tag}>'
+        return final
