307e96e73768374e5482970a39c479f158e78a1d
its-PKN-2k4 <phuckhanhnguyen2004@gmail.com>
2026-01-02 23:36:08 -0500

    Add testing methods for the text_to_textnode function

--- src/test_string_split_delimiter.py
+++ src/test_string_split_delimiter.py
@@ -114,3 +114,20 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes
         )
+
+    def test_text_to_textnodes(self):
+        text_to_nodes = text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD_TEXT),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC_TEXT),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE_TEXT),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ], text_to_nodes
+        )
