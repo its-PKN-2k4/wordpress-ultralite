72884b1e79c0e2dac5ae6951e3d0e3d72baeaabf
its-PKN-2k4 <phuckhanhnguyen2004@gmail.com>
2026-01-09 02:20:06 -0500

    Modify main function and main script to publish generated html file as webpage

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from textnode import TextNode, TextType
 from copy_dir_to_dir import copy_dir_to_dir
+from page_generator import generate_page
 
 def main():
     #obj1 = TextNode("All the time you have to leave a space", TextType.LINK, "https://www.boot.dev")
     #print(TextNode.__repr__(obj1))
     copy_dir_to_dir("./static", "./public")
+    generate_page("./content/index.md", "./template.html", "./public/index.html")
+
 
 if __name__ == "__main__":
     main()
